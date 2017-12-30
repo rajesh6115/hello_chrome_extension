ecb4ccdb0148418d7a60527fadc104a10bf4ae07
Rajesh <sahoorajesh.d@gmail.com>
2017-12-30 11:54:48 +0530

    [hello] basic hello world

---
+++ js/popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var mybutton = document.getElementById('myButton');
+    mybutton.addEventListener('click', function(){
+        alert("Button Clicked");
+    });
+});
